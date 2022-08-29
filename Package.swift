6fe1ab77dbdc020070148192337882056385d22c
Danny Ruiz <ynnadrules@gmail.com>
2022-08-29 06:10:43 -0500

    Bump anycodable to next available version

--- Package.swift
+++ Package.swift
@@ -6,13 +6,13 @@ let package = Package(
     name: "JavaCoder",
     products:[
         .library(
-            name: "JavaCoder", 
+            name: "JavaCoder",
             targets:["JavaCoder"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/readdle/java_swift.git", .upToNextMinor(from: "2.1.5")),
-        .package(url: "https://github.com/readdle/swift-anycodable.git", .upToNextMinor(from: "1.0.2")),
+        .package(url: "https://github.com/readdle/swift-anycodable.git", .upToNextMinor(from: "1.1.0")),
     ],
     targets: [
         .target(name: "JavaCoder", dependencies: ["java_swift", "AnyCodable"], path: "Sources"),
